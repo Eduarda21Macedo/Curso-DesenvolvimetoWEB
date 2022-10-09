e2da8f768c6d2420ab9c6650b079564ccdf807c4
Eduarda Silva <96318733+Eduarda21Macedo@users.noreply.github.com>
2022-10-08 21:38:33 -0300

    DEVWEB

---
+++ Exercicios-Js/UsandoLEtemloop1 copy.js
@@ -0,0 +1,7 @@
+const funcs = []
+for (let i = 0;i < 10; i++){
+    funcs.push(function(){
+    })
+}
+funcs[2]()
+funcs[8]()
---
+++ Exercicios-Js/UsandoVaremLoop.js
@@ -0,0 +1,6 @@
+for (var i =0 ;i<10;i++){
+    console.log(i)
+}
+console.log('i=', i)
+
+//com let ele não aparecia.
