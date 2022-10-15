10d1ca073d33e2dcf12c8fe7467c4352f3e646a6
Eduarda Silva <96318733+Eduarda21Macedo@users.noreply.github.com>
2022-10-14 23:32:26 -0300

    DEVWEB

---
+++ Exercicios&Fundamentos/OperadoresAritmeticos.js
@@ -0,0 +1,9 @@
+const [a,b,c,d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtraçao = d-b
+const multiplicaçao = a * b
+const divisao = a / b
+const modulo = a % 2
+
+console.log(soma,subtraçao,multiplicaçao,divisao,modulo)
---
+++ Exercicios&Fundamentos/OperadoresUnarios.js
@@ -0,0 +1,8 @@
+let num1 = 1
+let num2  = 2 
+
+num1++
+num2++
+console.log(null)
+--null
+console.log(num1)
